76988464b8511f8da084ec0b8d2a47908a0708cd
FLU <valerio.massj@gmail.com>
2021-10-29 17:08:20 +0200

    odd or even ex done

--- paridispari/js/script.js
+++ paridispari/js/script.js
@@ -6,3 +6,52 @@ Sommiamo i due numeri
 Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
 Dichiariamo chi ha vinto.
 */
+
+/* 
+-creo un prompt per chiedere all'utente di scrivere pari o dispari ok
+-creo un prompt per chiedere all'utente di scrivere un numero da 1 a 5 ok
+-creo una funzione ok
+    -creo una variabile che mi permette di creare un numero random da 1 a 5 ok
+-creo una variabile che sommi il numero inserito dall'utente con quello generato dal pc ok
+-creo una funzione
+    -creo una variabile stringa = ""
+    -se la variabile di somma e' divisibile per 2 resto 0
+        -la variabile stringa = "pari"
+    -altrimenti
+        -la variabile stringa = "dispari"
+-se la variabile stringa == prima variabile prompt utente
+    -stampo ha vinto l'utente: il numero e' ${variabile stringa}
+-altrimenti
+    -stampo ha vinto il pc: il numero e' ${variabile stringa}
+*/
+
+const userString = prompt("Choose odd or even");
+const userNum = parseInt(prompt("Choose a number from 1 to 5"));
+let pcNum = randomNum(5);
+
+
+function randomNum (max){
+    let pcNumFun= Math.floor(Math.random()* max +1);
+    return pcNumFun;
+}
+
+const total = userNum + pcNum;
+
+function oddEven(){
+    let stringFun = "";
+    if(total % 2 === 0){
+        stringFun ="even";
+    }else{
+        stringFun ="odd";
+    }
+    return stringFun
+}
+let string = oddEven();
+console.log(string);
+console.log(pcNum);
+
+if(string === userString){
+    console.log(`User won the numer was : ${total} wich is ${string}` );
+}else{
+    console.log(`Pc won the numer was : ${total} wich is ${string}` );
+}
